aacd4167a7bf635f6702527d2b9be9d6dd85a419
Tanmay Patil <patil.tanmay0697@gmail.com>
2024-06-30 18:46:04 -0700

    Add download logic

--- download.go
+++ download.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/tanmay-e-patil/orpheus-api/internal/database"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"os/exec"
 	"sync"
 	"time"
 )
+
+const BASE_DIR = "./public/library"
 
 func startScraping(db *database.Queries, concurrency int, timeBetweenRequest time.Duration) {
 	log.Printf("Collecting feeds every %s on %v goroutines...", timeBetweenRequest, concurrency)
@@ -27,6 +34,138 @@ func startScraping(db *database.Queries, concurrency int, timeBetweenRequest tim
 		}
 		wg.Wait()
 	}
+}
+
+func createSongDirectories(dirPath string) error {
+	// Check if the directory already exists
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		// Create the directory along with any necessary parents
+		err := os.MkdirAll(dirPath, 0755)
+		if err != nil {
+			fmt.Println("Error creating directory:", err)
+			return err
+		}
+		fmt.Println("Directory created:", dirPath)
+	} else {
+		fmt.Println("Directory already exists:", dirPath)
+	}
+	return nil
+
+}
+
+func downloadCoverArt(song database.Song) (string, error) {
+	// Create the file
+
+	outputFilePath := fmt.Sprintf("%s/%s/%s/cover_art.jpeg", BASE_DIR, song.ArtistName, song.AlbumName)
+	file, err := os.Create(outputFilePath)
+	if err != nil {
+		fmt.Printf("Error creating file: %v\n", err)
+		return "", err
+	}
+	defer file.Close()
+
+	// Get the data
+	resp, err := http.Get(song.AlbumArt)
+	if err != nil {
+		fmt.Printf("Error downloading file: %v\n", err)
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Bad status: %s\n", resp.Status)
+		return "", err
+	}
+
+	// Writer the body to file
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
+		return "", err
+	}
+	return outputFilePath, nil
+}
+
+func downloadSong(song database.Song) error {
+	// Create appropriate directories for song using BASE_DIR/ArtistName/AlbumName/
+	songDirPath := fmt.Sprintf("%s/%s/%s", BASE_DIR, song.ArtistName, song.AlbumName)
+	err := createSongDirectories(songDirPath)
+	if err != nil {
+		log.Printf("Error creating song directories: %v", err)
+		return err
+	}
+	// Download the song in that directory
+	// example cli command: yt-dlp -f bestaudio --extract-audio --audio-format flac --audio-quality 0
+	// xfqBQ2XhBCg -o SPOT! --postprocessor-args
+	//"ffmpeg:-metadata title='SPOT!' -metadata artist='ZICO, JENNIE' -metadata album='SPOT!' -metadata date='2024' -metadata year='2024'"
+
+	songFilePath := songDirPath + fmt.Sprintf("/tmpSong")
+
+	songMetadata := fmt.Sprintf("ffmpeg:-metadata title='%s' -metadata artist='%s' -metadata album='%s' -metadata date='%v' -metadata year='%v'",
+		song.Name, song.ArtistName, song.AlbumName, song.ReleaseDate.Year(), song.ReleaseDate.Year())
+
+	ytDlpCmd := exec.Command("yt-dlp",
+		"-f", "bestaudio",
+		"--extract-audio",
+		"--audio-format", "m4a",
+		"--audio-quality", "0",
+		fmt.Sprintf(song.VideoID),
+		"-o", songFilePath,
+		"--postprocessor-args",
+		songMetadata,
+	)
+
+	ytDlpCmd.Stdout = os.Stdout
+	ytDlpCmd.Stderr = os.Stderr
+
+	log.Printf("Downloading and converting audio...")
+	log.Printf("%v", ytDlpCmd.Args)
+	err = ytDlpCmd.Run()
+	if err != nil {
+		fmt.Printf("Error running yt-dlp command: %v\n", err)
+		return err
+	}
+
+	// Download the cover art
+	coverArtPath, err := downloadCoverArt(song)
+	if err != nil {
+		log.Printf("Error downloading cover art: %v\n", err)
+	}
+
+	// Add cover art to m4a file
+	// ffmpeg -i spot.flac -i spot-cover.jpeg -map 0 -map 1 -c:a copy -disposition:v attached_pic -metadata:s:v
+	//title="Album cover" -metadata:s:v comment="Cover (front)" output.m4a
+	log.Printf("Downloading cover art: %s\n", coverArtPath)
+
+	finalSongFilePath := songDirPath + fmt.Sprintf("/%s.m4a", song.Name)
+
+	ffmpegCmd := exec.Command("ffmpeg",
+		"-i", songFilePath+".m4a",
+		"-i", coverArtPath,
+		"-map_metadata", "0",
+		"-map", "0",
+		"-map", "1",
+		"-c", "copy",
+		"-disposition:v", "attached_pic",
+		finalSongFilePath)
+
+	ffmpegCmd.Stdout = os.Stdout
+	ffmpegCmd.Stderr = os.Stderr
+
+	log.Printf("Adding album art...")
+	log.Printf("%v", ffmpegCmd.Args)
+	err = ffmpegCmd.Run()
+	if err != nil {
+
+		log.Printf("Error running ffmpeg command: %v\n", err)
+		return err
+	}
+
+	log.Printf("Audio download, conversion, and metadata embedding completed successfully!")
+
+	return nil
+
 }
 
 func scrapeSongs(db *database.Queries, wg *sync.WaitGroup, song database.Song) {
